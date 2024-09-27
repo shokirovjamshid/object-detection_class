ad3c0c38718901997895ea1c07b3c0c61e47e148
shokirovjamshid <shokirovjamshid555@gmail.com>
2024-09-27 14:15:22 +0500

    model

---
+++ fasterrcnn_model.py
@@ -0,0 +1,12 @@
+from torchvision.io.image import read_image
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
+from torchvision.utils import draw_bounding_boxes
+from torchvision.transforms.functional import to_pil_image
+
+
+weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.9)
+model.eval()
+
+# Step 2: Initialize the inference transforms
+preprocess = weights.transforms()
